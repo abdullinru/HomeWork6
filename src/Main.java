9482e45dcc8529e697d5303694acf8091e6f2b43
abdullinru <abdullin17021989@gmail.com>
2022-05-03 17:06:50 +0600

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int []arr = generateRandomArray();
+        //System.out.println(Arrays.toString(arr));
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
